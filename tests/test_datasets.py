ae793eade5caee6fefbe7f4d9c8de13504aba4fa
Baptiste <baptiste.robertie@gmail.com>
2022-04-14 14:20:38 +0200

    Ajouter une fonction permettant de créer des données de train et de test pour une date d'évaluation t fixée

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,4 +1,4 @@
-from esgflib.data.datasets import get_melbourne_data
+from esgflib.data.datasets import get_melbourne_data, split_train_test_data
 
 
 def test_get_melbourne_data():
@@ -9,3 +9,16 @@ def test_get_melbourne_data():
     data = get_melbourne_data()
     # Check dataset is not empty
     assert len(data) > 0
+
+
+def test_split_train_test_data():
+    '''
+    Test the split_train_test_data function.
+    '''
+    # Get the data
+    data = get_melbourne_data()
+    # Split the data
+    train, test = split_train_test_data(data, split_year="1986")
+    # Check the train and test data are not empty
+    assert len(train) > 0
+    assert len(test) > 0
